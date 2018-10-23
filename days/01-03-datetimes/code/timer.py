9a949d39f06424fcd430237cdcb253be3a0c0057
Ryan Lang <Rylang.dev@gmail.com>
2018-10-23 16:25:11 -0500

    Small update to the timer module

--- days/01-03-datetimes/code/timer.py
+++ days/01-03-datetimes/code/timer.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-VALID_COMMANDS = ['help', 'lap', 'time', 'stop']
+VALID_COMMANDS = ['start', 'help', 'time', 'lap', 'stop']
 
 
 def sanitise_input(prompt, validation_string=None, type_=None):
@@ -22,15 +22,23 @@ def sanitise_input(prompt, validation_string=None, type_=None):
         return user_input.lower()
 
 
-def help_command():
-    help_string = "Full list of valid commands: \n\n"
-    for command in VALID_COMMANDS:
-        help_string += "{0}\n".format(command)
-    print(help_string)
-
-
-def time_command(elapsed_time):
-    print("Current elapsed time is: {0}".format(elapsed_time))
+# def help_command():
+#     help_string = "Full list of valid commands: \n\n"
+#     for command in VALID_COMMANDS:
+#         help_string += "{0}\n".format(command)
+#     print(help_string)
+#
+#
+# def time_command(elapsed_time):
+#     print("Current elapsed time is: {0}".format(elapsed_time))
+#
+#
+# def lap_command():
+#
+#
+#
+# def stop_command(elapsed_time):
+#     print("Total elapsed time is: {0}".format(elapsed_time))
 
 
 def main():
@@ -38,17 +46,34 @@ def main():
     sanitise_input("To begin the timer please type 'Start'.", "Start", str)
     start_time = datetime.now()
     print("Timer successfully started!")
+    laps = []
 
     # Request users next action
     while True:
         command = sanitise_input("Please enter a command or type 'Help' for a full list of commands.", type_=str)
-        if command == 'help':
-            help_command()
+        if command == 'start':
+            print("The timer has already started, please enter a different command.")
             continue
-        if command == 'time':
+        elif command == 'help':
+            help_string = "Full list of valid commands: \n\n"
+            for command in VALID_COMMANDS:
+                help_string += "{0}\n".format(command)
+            print(help_string)
+            continue
+        elif command == 'time':
             elapsed_time = str(datetime.now() - start_time)
-            time_command(elapsed_time)
+            print("Current elapsed time is: {0}".format(elapsed_time))
             continue
+        elif command == 'lap':
+            if not laps:
+                laps.append(str(datetime.now() - start_time))
+            else:
+                laps.append(str(datetime.now() - laps[:-1]))
+            print(laps)
+        elif command == 'stop':
+            elapsed_time = str(datetime.now() - start_time)
+            print("Total elapsed time is: {0}".format(elapsed_time))
+            break
         else:
             break
 
